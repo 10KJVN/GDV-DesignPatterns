d1ef26e1ebdf5f6c7814bcfd7f776e3782b57ed8
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-05-13 02:00:25 +0200

    Created LoadingScreen scripts, no logic yet.

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,22 @@
+public class LoadingScreen
+{
+    public LoadingScreen()
+    {
+        // ctor
+    }
+
+    internal void Show()
+    {
+        
+    }
+
+    internal void Hide()
+    {
+        
+    }
+
+    public void SetBarPercent(float percent)
+    {
+        
+    }
+}
