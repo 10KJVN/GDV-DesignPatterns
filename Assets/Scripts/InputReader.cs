8b276916ad6aa061d0508f2c555126c395df7ae8
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-06-06 22:50:55 +0200

    Add a whole lotta input methods, also using SO's

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -1,7 +1,8 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
-using static UnityEngine.InputSystem.DefaultInputActions;
+using static PlayerInputActions;
 
 public interface IInputReader
 {
@@ -9,35 +10,72 @@ public interface IInputReader
     void EnablePlayerActions();
 }
 
+[CreateAssetMenu(fileName = "InputReader", menuName = "InputReader")]
 public class InputReader : ScriptableObject, IPlayerActions, IInputReader
 {
     public event UnityAction<Vector2> Move = delegate { };
+    public event UnityAction<bool> Jump = delegate { };
+
+    public PlayerInputActions inputActions;
+    
+    public Vector2 Direction => inputActions.Player.Move.ReadValue<Vector2>();
+    public bool IsJumpKeyPressed => inputActions.Player.Jump.IsPressed();
+    
     public event UnityAction<Vector2, bool> Look = delegate { };
     public event UnityAction EnableMouseControlCamera = delegate { };
     public event UnityAction DisableMouseControlCamera = delegate { };
-    public event UnityAction<bool> Jump = delegate { };
     public event UnityAction<bool> Dash = delegate { };
     public event UnityAction Attack = delegate { };
     public event UnityAction<RaycastHit> Click = delegate { };
     
+    public void EnablePlayerActions()
+    {
+        if (inputActions == null)
+        {
+            inputActions = new PlayerInputActions();
+            inputActions.Player.SetCallbacks(this);
+        }
+        inputActions.Enable();
+    }
+    
     public void OnMove(InputAction.CallbackContext context)
     {
-        throw new System.NotImplementedException();
+        Move?.Invoke(context.ReadValue<Vector2>());
     }
 
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Started:
+                Jump?.Invoke(true);
+                break;
+            
+            case InputActionPhase.Canceled:
+                Jump?.Invoke(false);
+                break;
+            
+            case InputActionPhase.Disabled:
+            case InputActionPhase.Waiting:
+            case InputActionPhase.Performed:
+                break;
+            
+            default: throw new ArgumentOutOfRangeException();
+        }
+    }
+    
     public void OnLook(InputAction.CallbackContext context)
     {
-        throw new System.NotImplementedException();
+        // noop
     }
 
     public void OnFire(InputAction.CallbackContext context)
     {
-        throw new System.NotImplementedException();
+        // noop
     }
 
-    public Vector2 Direction { get; }
-    public void EnablePlayerActions()
+    public void OnRun(InputAction.CallbackContext context)
     {
-        throw new System.NotImplementedException();
+        // noop
     }
 }
