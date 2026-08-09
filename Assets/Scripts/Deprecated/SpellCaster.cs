872c8de7ee19824f242502c11d454fc004744c80
10KJVN <90614291+10KJVN@users.noreply.github.com>
2026-08-09 20:13:25 +0200

    Cleaned level of SpellCasters.
    A script i could convert to non MB, but not risking it with the amount of time left, its too irrelevant.

--- Assets/Scripts/Deprecated/SpellCaster.cs
+++ Assets/Scripts/Deprecated/SpellCaster.cs
@@ -5,14 +5,7 @@ public class SpellCaster : MonoBehaviour
 {
     [SerializeField] private SpellFactory _spellFactory;
     private ISpell _spell = ISpell.CreateDefault();
-
-    // private SpellCaster()
-    // {
-    //     _spellFactory = new FireFactory();
-    //     _spell = _spellFactory?.CreateSpell();
-    // }
-
-    // TODO: Rethink auto-assigning an element on start.
+    
     private void Start()
     {
         _spell = _spellFactory?.CreateSpell(); // Assigns selected Factory spell
